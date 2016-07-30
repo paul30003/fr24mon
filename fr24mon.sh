8913ecc4837b2d80203be32a54a78a366b07f23e
paul30003 <paul30003@hotmail.com>
2016-07-30 16:37:50 +0100

    Add files via upload

---
+++ fr24mon.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+###########################################################################
+# Originally written by: Paul Fryer, 2016
+# https://github.com/
+# License: GPL (>= 2.1) [http://www.gnu.org/licenses/gpl.html]
+###########################################################################
+
+# A couple of variables
+
+PIDFILE=/var/run/fr24feed.pid
+
+secret="xxxxxxxxxxxxxxxxxxxxxxxx"
+
+
+# Test if FR24FEED is running
+
+if [ -e "$PIDFILE" ];
+then
+
+ status_msg="FR24FEED Running"
+
+else
+ status_msg="FR24FEED Stopped"
+fi
+
+
+# Get Pi Data
+
+tempC=`/opt/vc/bin/vcgencmd measure_temp`
+running=`uptime -p`
+
+# Send to IFTTT
+
+json="{\"value1\":\"${tempC:5}\",\"value2\":\"${status_msg}\",\"value3\":\"${running:2}\"}"
+
+curl -X POST -H "Content-Type: application/json" -d "${json}" https://maker.ifttt.com/trigger/feeder_status/with/key/${secret}
